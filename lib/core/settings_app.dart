31d8351884dab99f90f603e569066aeb7ad7478a
Felipe Bellini <felipe.bellini@krykto.com>
2024-06-03 08:50:00 -0300

    settings_app.dart

---
+++ lib/core/settings_app.dart
@@ -0,0 +1,4 @@
+abstract class SettingsApp {
+  static const String baseApi = "https://dev3.impactoprime.com.br";
+  static const Map<String, String> headerDefaultSecurity = {"Authorization": " Basic WjxaR0Y/KWFAOzpXJTQwLkdjZkBn"};
+}
